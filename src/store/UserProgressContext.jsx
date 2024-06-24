699430cecacd1c1bf986bbb4b8fc27c8275b11aa
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-06-24 14:54:49 +0700

    Opening the Cart in the Modal via a New Context

---
+++ src/store/UserProgressContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState } from "react";
+
+const UserProgressContext = createContext({
+  progress: "",
+  showCart: () => {},
+  hideCart: () => {},
+  showCheckOut: () => {},
+  hideCheckOut: () => {},
+});
+
+export function UserProgressContextProvider({ children }) {
+  const [userProgress, setUserProgress] = useState();
+  function showCart() {
+    setUserProgress("cart");
+  }
+  function hideCart() {
+    setUserProgress("");
+  }
+  function showCheckOut() {
+    setUserProgress("checkout");
+  }
+  function hideCheckOut() {
+    setUserProgress("");
+  }
+  const UserProgressCtx = {
+    progress: userProgress,
+    showCart: showCart,
+    hideCart: hideCart,
+    showCheckOut: showCheckOut,
+    hideCheckOut: hideCheckOut,
+  };
+  return (
+    <UserProgressContext.Provider value={UserProgressCtx}>
+      {children}
+    </UserProgressContext.Provider>
+  );
+}
+
+export default UserProgressContext;
